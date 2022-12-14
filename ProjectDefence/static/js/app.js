ee0caef8a8590f7a0fa726227ed3aecf93a5859e
ganukov <ganukoff@gmail.com>
2022-12-14 18:03:54 +0000

    adjusted js files

--- ProjectDefence/static/js/app.js
+++ ProjectDefence/static/js/app.js
@@ -6,10 +6,7 @@
     // Launch Functions
     Launch: function () {
       fn.Header();
-      fn.OwlCarousel();
       fn.ImageView();
-      fn.Swiper();
-      fn.Masonry();
       fn.Apps();
     },
 
@@ -21,52 +18,7 @@
       });
     },
 
-    // owlcarousel
-    OwlCarousel: function() {
 
-      $('.owl-carousel').each(function() {
-        var a = $(this),
-          items = a.data('items') || [1,1,1,1],
-          margin = a.data('margin'),
-          loop = a.data('loop'),
-          nav = a.data('nav'),
-          dots = a.data('dots'),
-          center = a.data('center'),
-          speed = a.data('speed'),
-          autoplay = a.data('autoplay'),
-          autoplayTimeout = a.data('autoplaytimeout'),
-          autoplaySpeed = a.data('autoplayspeed'),
-          rtl = a.data('rtl'),
-          autoheight = a.data('autoheight'),
-          thumbs = a.data('thumbs');
-
-        var options = {
-          nav: nav || false,
-          loop: loop || false,
-          dots: dots || false,
-          center: center || false,
-          autoplay: autoplay || false,
-          autoHeight: autoheight || false,
-          rtl: rtl || false,
-          margin: margin || 0,
-          navSpeed: speed || 400,
-          dotsSpeed: speed || 400,
-          autoplayTimeout: autoplayTimeout || 3000,
-          autoplaySpeed: autoplaySpeed || 400,
-          autoplayHoverPause: true,
-          thumbs: thumbs || false,
-          thumbsPrerendered: thumbs || false,
-          responsive: {
-            0: { items: items[3] || 1 },
-            992: { items: items[2] || 1 },
-            1200: { items: items[1] || 1 },
-            1600: { items: items[0] || 1}
-          }
-        };
-
-        a.owlCarousel(options);
-      });
-    },
 
 
     // image view
@@ -95,108 +47,12 @@
     },
 
 
-    // swiper
-    Swiper: function() {
 
-      $('.swiper-container').each(function(index, element){
-        var $this = $(this)
-
-        $this.find(".swiper-pagination").addClass("swiper-pagination-" + index);
-        $this.find(".swiper-button-next").addClass("swiper-button-next-" + index);
-        $this.find(".swiper-button-prev").addClass("swiper-button-prev-" + index);
-
-        var options = {
-          parallax: true,
-          speed: 1500,
-          loop: true,
-          simulateTouch: false,
-          effect: 'fade',
-
-          //pagination
-          pagination: {
-            el: '.swiper-pagination-' + index,
-            clickable: true
-          },
-
-          // navigation
-          navigation: {
-            nextEl: '.swiper-button-next-' + index,
-            prevEl: '.swiper-button-prev-' + index,
-          }
-
-        };
-
-        var slider = $(this);
-
-        if ($(this).hasClass('swiper-container-carousel')) {
-          options.spaceBetween = 10;
-          options.effect = 'slide';
-          options.simulateTouch = true;
-          options.slideToClickedSlide = true;
-        }
-
-        new Swiper ( slider, options );
-      });
-
-
-
-      if ( $( ".gallery-container" ).length ) {
-        var galleryTop = new Swiper('.gallery-container', {
-          effect: 'fade',
-          speed: 1500,
-          simulateTouch: false
-        });
-        var galleryThumbs = new Swiper('.gallery-thumbs', {
-          centeredSlides: true,
-          slidesPerView: 6,
-          speed: 1500,
-          breakpoints: {
-            1600: { slidesPerView: 5 },
-            1200: { slidesPerView: 3 },
-            768: { slidesPerView: 2 },
-            576: { slidesPerView: 2 }
-          },
-          slideToClickedSlide: true
-        });
-        galleryTop.controller.control = galleryThumbs;
-        galleryThumbs.controller.control = galleryTop;
-      }
-
-    },
-
-
-    // masonry
-    Masonry: function() {
-      var $grid = $('.masonry').masonry({
-        itemSelector: '.masonry > *',
-      });
-
-      $grid.imagesLoaded().progress( function() {
-        $grid.masonry('layout');
-      });
-    },
-
-
-    // sidebar
-    Sidebar: function() {
-      var sidebar_open = $(".sidebar-open");
-      var sidebar = $(".sidebar")
-      var sidebar_close = $(".sidebar-close");
-      sidebar_open.click( function () {
-          if( $(this).hasClass("open") ) {
-             $(this).removeClass("open");
-          } 
-          else {
-              speaker.removeClass("open");
-              $(this).addClass("open");
-          }
-      });
-    },
 
 
     // apps
     Apps: function () {
-      
+
       // lavalamp
       $('.lavalamp').lavalamp({
         setOnClick: true,
@@ -217,96 +73,6 @@
       });
 
 
-      // range slider
-      $('.rangeslider').each(function() {
-        var a = $(this),
-          min = a.data('min'),
-          max = a.data('max'),
-          from = a.data('from'),
-          to = a.data('to')
-
-        var options = {
-          type: "double",
-          min: min || 0,
-          max: max || 1000,
-          from: from || 200,
-          to: to || 800,
-          prefix: "$"
-        };
-
-        a.ionRangeSlider(options);
-      });
-
-      // counter
-      $(document).ready(function(){
-
-          $('.counter-plus').click(function(){
-
-              var fieldID = $(this).attr('field'),
-                  fieldVal = parseInt($('input[name='+fieldID+']').val());
-
-              if (!isNaN(fieldVal)) {
-                  $('input[name='+fieldID+']').val(fieldVal + 1);
-              } else {
-                  $('input[name='+fieldID+']').val(0);
-              }
-          });
-
-          $(".counter-minus").click(function() {
-
-              var fieldID = $(this).attr('field'),
-                  fieldVal = parseInt($('input[name='+fieldID+']').val());
-
-              if (!isNaN(fieldVal) && fieldVal > 0) {
-                  $('input[name='+fieldID+']').val(fieldVal - 1);
-              } else {
-                  $('input[name='+fieldID+']').val(0);
-              }
-          });
-      });
-
-
-
-
-      //  accordion with radio buttons
-      $(".accordion-radio").on('click', function(){
-          $(this).next('input').prop("checked", true);
-      });
-
-      // accordion active class
-      $('.collapse').on('show.bs.collapse', function () {
-        $(this).parent().addClass('active');
-      });
-
-      $('.collapse').on('hide.bs.collapse', function () {
-        $(this).parent().removeClass('active');
-      });
-
-
-      // skrollr
-      skrollr.init({
-          forceHeight: false,        
-          mobileCheck: function() {
-              //hack - forces mobile version to be off
-              return false;
-          }
-      });
-
-
-
-      // tooltips
-      $('[data-toggle="tooltip"]').tooltip();
-
-
-      // timer
-      $('#timer').countdown('2020/01/01', function(event) {
-          var $this = $(this).html(event.strftime(''
-              + '<div>%D <span>days</span></div> '
-              + '<div>%H <span>hours</span></div> '
-              + '<div>%M <span>min</span></div> '
-              + '<div>%S <span>sec</span></div>'));
-      });
-
 
       // smooth scroll
       $(function () {
@@ -314,17 +80,10 @@
       });
 
 
-      // mobile menu open state
-      $(document).ready(function(){
-        $('.navbar-toggler').click(function(){
-          $('body').toggleClass('overlay-active');
-        });
-      });
-
 
       // zoom
       $('.zoom').zoom();
-      // $('.zoom-click').zoom({ on:'click' }); 
+      // $('.zoom-click').zoom({ on:'click' });
 
 
       // sub menu
